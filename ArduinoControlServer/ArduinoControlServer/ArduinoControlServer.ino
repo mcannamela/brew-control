9a5ab34761c36fb7b4678f07b076fa41d8c904ba
michael <mcannamela>
2016-01-14 21:07:55 -0500

    refactor web commands to separate file

--- ArduinoControlServer/ArduinoControlServer/ArduinoControlServer.ino
+++ ArduinoControlServer/ArduinoControlServer/ArduinoControlServer.ino
@@ -35,11 +35,14 @@
 #include "avr/pgmspace.h" // new include
 #include "Ethernet.h"
 #include <WebServer.h>
+
 //#include "BrewControl.h"
 #include "PinCommand.h"
+#include "Interrupts.h"
+#include "Requests.h"
 
 
-#define VERSION_STRING "0.1"
+
 /* copy-pasta from brewControlServer
 byte mac[] = { 
   0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
@@ -61,26 +64,7 @@ static uint8_t mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
  * DHCP from your router. */
 static uint8_t ip[] = { 192, 168, 11, 101 };
 
-// ROM-based messages used by the application
-// These are needed to avoid having the strings use up our limited
-//    amount of RAM.
 
-P(Page_start) = "<html><head><title>Web_Parms_1 Version " VERSION_STRING "</title></head><body>\n";
-P(Page_end) = "</body></html>";
-P(Get_head) = "<h1>GET from ";
-P(Post_head) = "<h1>POST to ";
-P(Unknown_head) = "<h1>UNKNOWN request for ";
-P(Default_head) = "unidentified URL requested.</h1><br>\n";
-P(Raw_head) = "raw.html requested.</h1><br>\n";
-P(Parsed_head) = "parsed.html requested.</h1><br>\n";
-P(Good_tail_begin) = "URL tail = '";
-P(Bad_tail_begin) = "INCOMPLETE URL tail = '";
-P(Tail_end) = "'<br>\n";
-P(Parsed_tail_begin) = "URL parameters:<br>\n";
-P(Parsed_item_separator) = " = '";
-P(Params_end) = "End of parameters<br>\n";
-P(Post_params_begin) = "Parameters sent by POST:<br>\n";
-P(Line_break) = "<br>\n";
 
 
 
@@ -88,176 +72,6 @@ P(Line_break) = "<br>\n";
  * of "", all pages will be at the root of the server. */
 #define PREFIX ""
 WebServer webserver(PREFIX, 80);
-
-
-
-/* commands are functions that get called by the webserver framework
- * they can read any posted data from client, and they output to the
- * server to send data back to the web browser. */
-void helloCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
-{
-  /* this line sends the standard "we're all OK" headers back to the
-     browser */
-  server.httpSuccess();
-
-  /* if we're handling a GET or POST, we can output our data here.
-     For a HEAD request, we just stop after outputting headers. */
-  if (type == WebServer::HEAD)
-    return;
-
-  server.printP(Page_start);
-  switch (type)
-    {
-    case WebServer::GET:
-        server.printP(Get_head);
-        break;
-    case WebServer::POST:
-        server.printP(Post_head);
-        break;
-    default:
-        server.printP(Unknown_head);
-    }
-
-    server.printP(Default_head);
-    server.printP(tail_complete ? Good_tail_begin : Bad_tail_begin);
-    server.print(url_tail);
-    server.printP(Tail_end);
-    server.printP(Page_end);
-
-}
-
-
-void rawCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
-{
-  /* this line sends the standard "we're all OK" headers back to the
-     browser */
-  server.httpSuccess();
-
-  /* if we're handling a GET or POST, we can output our data here.
-     For a HEAD request, we just stop after outputting headers. */
-  if (type == WebServer::HEAD)
-    return;
-
-  server.printP(Page_start);
-  switch (type)
-    {
-    case WebServer::GET:
-        server.printP(Get_head);
-        break;
-    case WebServer::POST:
-        server.printP(Post_head);
-        break;
-    default:
-        server.printP(Unknown_head);
-    }
-
-    server.printP(Raw_head);
-    server.printP(tail_complete ? Good_tail_begin : Bad_tail_begin);
-    server.print(url_tail);
-    server.printP(Tail_end);
-    server.printP(Page_end);
-
-}
-
-
-
-void parsedCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
-{
-  URLPARAM_RESULT rc;
-  char name[NAMELEN];
-  char value[VALUELEN];
-
-  /* this line sends the standard "we're all OK" headers back to the
-     browser */
-  server.httpSuccess();
-
-  /* if we're handling a GET or POST, we can output our data here.
-     For a HEAD request, we just stop after outputting headers. */
-  if (type == WebServer::HEAD)
-    return;
-
-  server.printP(Page_start);
-  switch (type)
-    {
-    case WebServer::GET:
-        server.printP(Get_head);
-        break;
-    case WebServer::POST:
-        server.printP(Post_head);
-        break;
-    default:
-        server.printP(Unknown_head);
-    }
-
-    server.printP(Parsed_head);
-    server.printP(tail_complete ? Good_tail_begin : Bad_tail_begin);
-    server.print(url_tail);
-    server.printP(Tail_end);
-
-  if (strlen(url_tail))
-    {
-    server.printP(Parsed_tail_begin);
-    while (strlen(url_tail))
-      {
-      rc = server.nextURLparam(&url_tail, name, NAMELEN, value, VALUELEN);
-      if (rc == URLPARAM_EOS)
-        server.printP(Params_end);
-       else
-        {
-        server.print(name);
-        server.printP(Parsed_item_separator);
-        server.print(value);
-        server.printP(Tail_end);
-        }
-      }
-    }
-  if (type == WebServer::POST)
-  {
-    server.printP(Post_params_begin);
-    while (server.readPOSTparam(name, NAMELEN, value, VALUELEN))
-    {
-      server.print(name);
-      server.printP(Parsed_item_separator);
-      server.print(value);
-      server.printP(Tail_end);
-    }
-  }
-  server.printP(Page_end);
-
-}
-
-void my_failCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
-{
-  /* this line sends the "HTTP 400 - Bad Request" headers back to the
-     browser */
-  server.httpFail();
-
-  /* if we're handling a GET or POST, we can output our data here.
-     For a HEAD request, we just stop after outputting headers. */
-  if (type == WebServer::HEAD)
-    return;
-
-  server.printP(Page_start);
-  switch (type)
-    {
-    case WebServer::GET:
-        server.printP(Get_head);
-        break;
-    case WebServer::POST:
-        server.printP(Post_head);
-        break;
-    default:
-        server.printP(Unknown_head);
-    }
-
-    server.printP(Default_head);
-    server.printP(tail_complete ? Good_tail_begin : Bad_tail_begin);
-    server.print(url_tail);
-    server.printP(Tail_end);
-    server.printP(Page_end);
-
-}
-
 
 
 
