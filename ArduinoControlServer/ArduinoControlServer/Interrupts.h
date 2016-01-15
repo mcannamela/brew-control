ea6cb6432588ad39ffc17a1a44858d42089aec5e
michael <mcannamela>
2016-01-14 21:08:07 -0500

    implement interrupt handlers

--- ArduinoControlServer/ArduinoControlServer/Interrupts.h
+++ ArduinoControlServer/ArduinoControlServer/Interrupts.h
@@ -1,5 +1,47 @@
 #include "Constants.h"
 
-unsigned long _LAST_INTERRUPT_TIME = 0;
-double _MEAN_INTERRUPT_TIME[N_INTERRUPT_PINS];
-double _N_INTERRUPTS_IN_AVERAGE;
+#ifndef INTERRUPTS_H
+#define INTERRUPTS_H
+
+volatile unsigned long _LAST_INTERRUPT_TIME[N_INTERRUPT_PINS];
+volatile double _MEAN_INTERRUPT_TIME[N_INTERRUPT_PINS];
+const double _DECAY_WEIGHT = .95;
+
+
+void initInterruptTimeArrays() {
+  for (int i = 0; i < N_INTERRUPT_PINS; i) {
+    _LAST_INTERRUPT_TIME[i] = 0;
+    _MEAN_INTERRUPT_TIME[i] = 0.0;
+  }
+}
+
+
+double updateAverage(double previousAverage, double thisPoint) {
+  return _DECAY_WEIGHT * previousAverage + (1.0 - _DECAY_WEIGHT) * thisPoint;
+}
+
+
+
+void handleInterrupt(int interruptNr) {
+  unsigned long t = millis();
+  unsigned long delta;
+
+  // only update if the clock has not overflowed
+  if (t >= _LAST_INTERRUPT_TIME[interruptNr]) {
+    delta = t - _LAST_INTERRUPT_TIME[interruptNr];
+    _MEAN_INTERRUPT_TIME[interruptNr] = updateAverage(_MEAN_INTERRUPT_TIME[interruptNr], (double) delta);
+  }
+  
+  _LAST_INTERRUPT_TIME[interruptNr] = t;
+}
+
+
+void handleInterruptZero() {
+  handleInterrupt(0);
+}
+
+void handleInterruptOne() {
+  handleInterrupt(1);
+}
+
+#endif
