b33669f963729b817bbb7a2c473eb1ed3dafb4a1
michael <mcannamela>
2016-01-15 23:00:10 -0500

    Test interrupt times decay over time

---
+++ BrewControl/examples/testInterruptsDecayOverTime/testInterruptsDecayOverTime.ino
@@ -0,0 +1,24 @@
+#include <Interrupts.h>
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("BEGIN SETUP");
+  pinMode(LED_PIN, OUTPUT);
+  initInterruptTimeArrays();
+  pinMode(INTERRUPT_PINS[0], OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PINS[0]), handleInterruptZero, RISING);
+  pulseInterruptRising(0, 1000);
+  Serial.println("END SETUP");
+}
+
+void loop()
+{
+  Serial.println("\n\nLOOP");
+  delay(10);
+  digitalWrite(LED_PIN, HIGH);
+  Serial.print("Mean interrupt time after ");
+  Serial.print(millis());
+  Serial.println(getMeanInterruptTime(0));
+  delay(250);
+}
