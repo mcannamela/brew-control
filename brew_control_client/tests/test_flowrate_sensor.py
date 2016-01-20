ef8362616acc54fb088b5c68278b810ceba3be3e
michael <mcannamela>
2016-01-20 18:58:45 -0500

    tests for FlowrateSensor

---
+++ brew_control_client/tests/test_flowrate_sensor.py
@@ -0,0 +1,15 @@
+import unittest
+
+from flowrate_sensor import FlowrateSensor
+
+
+class TestFlowrateSensor(unittest.TestCase):
+    def setUp(self):
+        self._liters_per_pulse = 5.0
+        self._pulse_frequency = 10.0
+        self._exp_flowrate = 50.0
+
+        self._flowrate_sensor = FlowrateSensor(self._liters_per_pulse)
+
+    def test_get_flowrate(self):
+        self.assertAlmostEqual(self._flowrate_sensor.get_flowrate(self._pulse_frequency), self._exp_flowrate)
