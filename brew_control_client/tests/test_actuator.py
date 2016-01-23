166462b98bfba544103f385fc8733e2265ba53ab
michael <mcannamela>
2016-01-23 15:31:39 -0500

    test for HLT actuator as well

--- brew_control_client/tests/test_actuator.py
+++ brew_control_client/tests/test_actuator.py
@@ -1,15 +1,16 @@
 import unittest
+from unittest import TestCase
+
 import mock
 
-from actuator import HEXActuator
+from actuator import HEXActuator, HLTActuator
 from brew_state import BrewState
 from interlock import Interlock
 from pin_command import CommandFactory
 from pin_config import PinConfig
 
 
-class TestHEXActuator(unittest.TestCase):
-
+class BaseHeaterActuatorTestCase(TestCase):
     def setUp(self):
         self._issue_command = mock.Mock()
         self._pin_config = PinConfig()
@@ -17,7 +18,7 @@ class TestHEXActuator(unittest.TestCase):
         self._interlock = mock.Mock(spec_set=Interlock)
         self._other_interlock = mock.Mock(spec_set=Interlock)
         self._interlocks = [self._interlock, self._other_interlock]
-        self._actuator = HEXActuator(self._issue_command, self._command_factory, self._interlocks)
+        self._actuator = self.get_actuator_constructor()(self._issue_command, self._command_factory, self._interlocks)
 
         self._brew_state = BrewState(
                 1.0,
@@ -28,7 +29,10 @@ class TestHEXActuator(unittest.TestCase):
                 True
         )
 
-    def test_actuate_with_interlocks_ok(self):
+    def _assert_command_issued(self, c):
+        self._issue_command.assert_called_once_with(c.render_as_request_parameters())
+
+    def _check_actuate_with_interlocks_ok(self):
         for lock in self._interlocks:
             lock.may_actuate.return_value = True
 
@@ -37,9 +41,10 @@ class TestHEXActuator(unittest.TestCase):
             lock.may_actuate.assert_called_once_with(self._brew_state)
             self.assertFalse(lock.may_deactuate.called)
 
-        self._issue_command.assert_called_once_with(self._command_factory.get_hex_on_command().render_as_request_parameters())
+        c = self._get_exp_on_command()
+        self._assert_command_issued(c)
 
-    def test_actuate_with_interlocks_faulted(self):
+    def _check_actuate_with_interlocks_faulted(self):
         for lock in self._interlocks:
             lock.may_actuate.return_value = False
             lock.may_deactuate.return_value = True
@@ -49,9 +54,10 @@ class TestHEXActuator(unittest.TestCase):
             lock.may_actuate.assert_called_once_with(self._brew_state)
             lock.may_deactuate.assert_called_once_with(self._brew_state)
 
-        self._issue_command.assert_called_once_with(self._command_factory.get_hex_off_command().render_as_request_parameters())
+        c = self._get_exp_off_command()
+        self._assert_command_issued(c)
 
-    def test_deactuate_with_interlocks_faulted(self):
+    def _check_deactuate_with_interlocks_faulted(self):
         for lock in self._interlocks:
             lock.may_actuate.return_value = False
             lock.may_deactuate.return_value = True
@@ -61,4 +67,55 @@ class TestHEXActuator(unittest.TestCase):
             self.assertFalse(lock.may_actuate.called)
             lock.may_deactuate.assert_called_once_with(self._brew_state)
 
-        self._issue_command.assert_called_once_with(self._command_factory.get_hex_off_command().render_as_request_parameters())
+        c = self._get_exp_off_command()
+        self._assert_command_issued(c)
+
+    def get_actuator_constructor(self):
+        raise NotImplementedError()
+
+    def _get_exp_on_command(self):
+        raise NotImplementedError()
+
+    def _get_exp_off_command(self):
+        raise NotImplementedError()
+
+
+class TestHEXActuator(BaseHeaterActuatorTestCase):
+
+    def test_actuate_with_interlocks_ok(self):
+        self._check_actuate_with_interlocks_ok()
+
+    def test_actuate_with_interlocks_faulted(self):
+        self._check_actuate_with_interlocks_faulted()
+
+    def test_deactuate_with_interlocks_faulted(self):
+        self._check_deactuate_with_interlocks_faulted()
+
+    def get_actuator_constructor(self):
+        return HEXActuator
+
+    def _get_exp_on_command(self):
+        return self._command_factory.get_hex_on_command()
+
+    def _get_exp_off_command(self):
+        return self._command_factory.get_hex_off_command()
+
+class TestHLTActuator(BaseHeaterActuatorTestCase):
+
+    def test_actuate_with_interlocks_ok(self):
+        self._check_actuate_with_interlocks_ok()
+
+    def test_actuate_with_interlocks_faulted(self):
+        self._check_actuate_with_interlocks_faulted()
+
+    def test_deactuate_with_interlocks_faulted(self):
+        self._check_deactuate_with_interlocks_faulted()
+
+    def get_actuator_constructor(self):
+        return HLTActuator
+
+    def _get_exp_on_command(self):
+        return self._command_factory.get_hlt_on_command()
+
+    def _get_exp_off_command(self):
+        return self._command_factory.get_hlt_off_command()
