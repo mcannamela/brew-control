30724345f988757fb6c94c0bd527ed6c4ccadf16
mcannamela <m.cannamela@gmail.com>
2017-06-18 09:04:26 -0400

    Enable flowrate interlock and plot flowrate in L/min

--- plot_brew.py
+++ plot_brew.py
@@ -2,16 +2,15 @@ import argparse
 from datetime import datetime
 import numpy as np
 from matplotlib import pyplot as plt
-#from scipy.signal import medfilt
+# from scipy.signal import medfilt
 
 from brew_control_client.brew_state import BrewState
 
 parser = argparse.ArgumentParser(description='Plot the brewlog.')
-parser.add_argument('brewfile',  type=str, nargs=1,
-                   help='Name of the brew')
+parser.add_argument('brewfile', type=str, nargs=1,
+                    help='Name of the brew')
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     args = parser.parse_args()
 
     data_filename = args.brewfile[0]
@@ -33,15 +32,15 @@ if __name__=="__main__":
     flowrates = np.array([s.pump_outlet_flowrate for s in brew_states])
 
     max_temp = np.max(temperatures)
-    hex_on = np.array([s.hex_actuated for s in brew_states])*.3*max_temp + .01*max_temp
-    hlt_on = np.array([s.hlt_actuated for s in brew_states])*.25*max_temp + .005*max_temp
+    hex_on = np.array([s.hex_actuated for s in brew_states]) * .3 * max_temp + .01 * max_temp
+    hlt_on = np.array([s.hlt_actuated for s in brew_states]) * .25 * max_temp + .005 * max_temp
 
     dtimes = [s.dtime for s in brew_states]
 
     temperatures = np.array(temperatures)
-    t = [(dt-dtimes[0]).total_seconds()/60.0 for dt in dtimes]
+    t = [(dt - dtimes[0]).total_seconds() / 60.0 for dt in dtimes]
     plt.figure(figsize=(14, 9))
-    plt.subplot(2,1,1)
+    plt.subplot(2, 1, 1)
     plt.plot(t, temperatures[:, 0], 'k', label='HLT')
     plt.plot(t, temperatures[:, 1], 'g', label='HEX Outlet')
     plt.plot(t, temperatures[:, 2], 'r', label='HEX Interlock')
@@ -53,10 +52,10 @@ if __name__=="__main__":
     plt.title('{}, t0={}'.format(data_filename, dtimes[0]))
     plt.legend(loc='upper left')
 
-    plt.subplot(2,1,2)
-    plt.plot(t, flowrates, 'k', label='Pump Outlet')
+    plt.subplot(2, 1, 2)
+    plt.plot(t, flowrates*60, 'k', label='Pump Outlet')
     plt.xlabel('time, min')
-    plt.ylabel('flowrate, L/s')
+    plt.ylabel('flowrate, L/min')
     plt.title('{}, t0={}'.format(data_filename, dtimes[0]))
     plt.legend(loc='upper left')
 
